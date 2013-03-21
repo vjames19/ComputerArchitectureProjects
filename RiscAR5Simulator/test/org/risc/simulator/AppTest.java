cecc4ff42f0bbeb42a192eac0647729e6eaed4b4
Victor J <victor.reventos1@gmail.com>
2013-03-21 17:41:15 -0400

    initial commit

---
+++ RiscAR5Simulator/test/org/risc/simulator/AppTest.java
@@ -0,0 +1,24 @@
+package org.risc.simulator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		App.main(null);
+	}
+
+}
